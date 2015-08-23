ecb11dcd66d23e99169f209a7626aeb4a0e72d8d
Willian Zhang <Willian@Willian-Zhang.com>
2015-08-23 13:58:42 +0800

    OWO

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+  var x = {
+    sucess: true,
+    session:'c3749879cdbcb79e7a6b95f3d241a784',
+    sessionTimeout:'3600',
+    friends:[
+      {
+        name:'mike'
+      },{
+        name:'alice'
+      }
+    ]
+
+  };
+  res.send(x);
 });
 
 module.exports = router;
